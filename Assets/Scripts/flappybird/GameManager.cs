234692346da4224ecb5a65c6200f2fc1cbad3ad7
rukinish <rukaiyanishfan@gmail.com>
2024-03-12 15:35:37 +0530

    databse functions

--- Assets/Scripts/flappybird/GameManager.cs
+++ Assets/Scripts/flappybird/GameManager.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using Firebase.Extensions;
+using Firebase.Firestore;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -116,5 +119,45 @@ public class GameManager : MonoBehaviour
     {
         score++;
         scoreText.text = score.ToString();
+    }
+
+      private string GenerateGameInstanceId()
+    {
+        long timestamp = System.DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        System.Random random = new System.Random();
+        int randomComponent = random.Next(10000);
+        string gameInstanceId = $"{timestamp}-{randomComponent}";
+        return gameInstanceId;
+    }
+
+    private void SaveGameDataToFirestore(string userId, string gameId, string gameInstanceId, int score)
+    {
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+
+        DocumentReference gameHistoryRef = db.Collection("users").Document(userId)
+            .Collection("game_history").Document(gameId).Collection("game_instances").Document(gameInstanceId);
+
+        Dictionary<string, object> gameData = new Dictionary<string, object>
+        {
+            { "score", score },
+            { "completionTime", FieldValue.ServerTimestamp },
+            { "date", DateTime.UtcNow } // Store the current date
+        };
+
+        gameHistoryRef.SetAsync(gameData).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsFaulted)
+            {
+                Debug.LogError("Failed to write game data to Firestore: " + task.Exception);
+            }
+            else if (task.IsCanceled)
+            {
+                Debug.LogError("Firestore write operation was cancelled");
+            }
+            else
+            {
+                Debug.Log("Game data successfully written to Firestore");
+            }
+        });
     }
 }
