d51441d72e935d61376678f12e60a4d83a7e02ae
rukinish <rukaiyanishfan@gmail.com>
2024-03-12 15:31:38 +0530

    database initialise, it works

--- Assets/Scripts/flappybird/GameManager.cs
+++ Assets/Scripts/flappybird/GameManager.cs
@@ -1,3 +1,4 @@
+using Firebase.Extensions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +11,9 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject getReadyImage; // Reference to the "Get Ready" image
     [SerializeField] private GameObject gameOverImage; // Reference to the "Game Over" image
     private BackgroundMusic backgroundMusic; 
+
+    private User user;
+    private Firebase.FirebaseApp app;
 
     private int score;
     public int Score => score;
@@ -37,6 +41,22 @@ public class GameManager : MonoBehaviour
         getReadyImage.SetActive(true);
         playButton.SetActive(true);
          gameOverImage.SetActive(false);
+
+         Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+        var dependencyStatus = task.Result;
+        if (dependencyStatus == Firebase.DependencyStatus.Available) {
+        // Create and hold a reference to your FirebaseApp,
+        // where app is a Firebase.FirebaseApp property of your application class.
+            app = Firebase.FirebaseApp.DefaultInstance;
+            Debug.Log("Firebase is ready to use!");
+
+        // Set a flag here to indicate whether Firebase is ready to use by your app.
+        } else {
+             UnityEngine.Debug.LogError(System.String.Format(
+             "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+            // Firebase Unity SDK is not safe to use here.
+        }
+    });
     }
 
     public void Play()
